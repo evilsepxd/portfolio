b9387dc0dc4949b85ee236b32657a3c4fe5210d3
Petr <evilsephy1@mail.ru>
2023-09-09 23:16:10 +0300

    reworked design & added some info about myself

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,41 @@
-const hamburger = document.querySelector('.hamburger'),
+document.addEventListener('DOMContentLoaded', () => {
+	const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
 	closeElem = document.querySelector('.menu__close'),
-	overlay = document.querySelector('.menu__overlay');
+	overlay = document.querySelector('.menu__overlay'),
+	counters = document.querySelectorAll('.metrics__persentage'),
+	lines = document.querySelectorAll('.metrics__progressbar span'),
+	typingTextElement = document.querySelector('#front-end');
 
-hamburger.addEventListener('click', () => {
-	menu.classList.add('active');
-	hamburger.classList.add('inactive');
-});
+	hamburger.addEventListener('click', () => {
+		menu.classList.add('active');
+		hamburger.classList.add('inactive');
+	});
 
-closeElem.addEventListener('click', () => {
-	menu.classList.remove('active');
-	hamburger.classList.remove('inactive');
-});
+	closeElem.addEventListener('click', () => {
+		menu.classList.remove('active');
+		hamburger.classList.remove('inactive');
+	});
 
-const counters = document.querySelectorAll('.metrics__persentage'),
-	lines = document.querySelectorAll('.metrics__progressbar span');
+	overlay.addEventListener('click', () => {
+		menu.classList.remove('active');
+		hamburger.classList.remove('inactive');
+	});
 
-counters.forEach( (item, i) => {
-	lines[i].style.width = item.innerHTML;
+	counters.forEach( (item, i) => {
+		lines[i].style.width = item.innerHTML;
+	});
+
+	function makeTypingText(elem, time) {
+		const text = elem.textContent;
+		elem.textContent = ' ';
+		
+		for (let i = 0; i < text.length; i++) {
+			setTimeout(sign => {
+				elem.textContent += sign;
+			}, time * i, text[i]);
+		}
+	}
+
+	makeTypingText(typingTextElement, 200);
 });
